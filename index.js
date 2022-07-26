03e8a5e98a0bada73700b85b905c3a5b4fa5a785
Dharmendra-ojha <96345964+Dharmendra-ojha@users.noreply.github.com>
2022-07-26 06:58:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
 var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
 var weather = document.querySelector('#weather')
  
 apik = "76f59a5ff3bd295292bbb8f3475442f9"
